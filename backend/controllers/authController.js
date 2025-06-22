a7382494ce54c344dbf578a106b2ff4fd6681a95
himanshusharma2007 <sharmahimanshu6478@gmail.com>
2025-06-22 21:16:24 +0530

    auth apis tested

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,3 @@
-
 // controllers/authController.js
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
@@ -13,8 +12,17 @@ exports.signup = async (req, res) => {
     await user.save();
 
     const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000
+    });
+
     res.status(201).json({ token, user: { id: user._id, name, email, isAdmin: user.isAdmin } });
   } catch (error) {
+    console.log('error', error)
     res.status(500).json({ msg: 'Server error' });
   }
 };
@@ -29,17 +37,31 @@ exports.login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000
+    });
+
     res.json({ token, user: { id: user._id, name: user.name, email, isAdmin: user.isAdmin } });
   } catch (error) {
     res.status(500).json({ msg: 'Server error' });
   }
 };
+
 exports.getProfile = async (req, res) => {
-    try {
-      const user = await User.findById(req.user.id).select('-password');
-      if (!user) return res.status(404).json({ msg: 'User not found' });
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ msg: 'Server error' });
-    }
-  };
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) return res.status(404).json({ msg: 'User not found' });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
+
+exports.logout = (req, res) => {
+  res.cookie('token', '', { maxAge: 1 });
+  res.json({ msg: 'Logged out successfully' });
+};
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 exports.protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  
   if (!token) return res.status(401).json({ msg: "No token" });
 
   try {
