91d82534ea5e161516885c8f60661b6fe1778b68
Dhairya jain <dhairyajain2617@gmail.com>
2025-06-24 21:43:58 +0530

    deploy with frontend link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://civicfix-hilv.onrender.com",
+  origin: "http://localhost:5174",
   credentials: true, // Allow cookies to be sent with requests
 }));
 app.use(express.json());
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://civicfix-hilv.onrender.com/";
