00b63225932cba1dd493aa7f3357cfdbe6e81610
himanshusharma2007 <sharmahimanshu6478@gmail.com>
2025-06-24 22:23:48 +0530

    fixing deployemtent errors /2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,9 +10,10 @@ const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 && window.location.pathname !== '/login') {
       // Redirect to login page
-      window.location.href = '/login';
+      // window.location.href = '/login';
+      console.log("error response", error.response);
     }
     return Promise.reject(error);
   }
