da2560d5ef95dd98eae7de1067d47c3f6e5f3cb2
himanshusharma2007 <sharmahimanshu6478@gmail.com>
2025-06-24 21:54:27 +0530

    frontend link seted up  in cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: "https://civic-fix-tau.vercel.app",
   credentials: true, // Allow cookies to be sent with requests
 }));
 app.use(express.json());
